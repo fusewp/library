26867cd0188aebecd174ae03d1812938cdb696f8
Collins Agbonghama <Collizo4sky@gmail.com>
2024-03-16 11:19:34 +0100

    Update add-delay-between-bulk-sync-requests.php

--- add-delay-between-bulk-sync-requests.php
+++ add-delay-between-bulk-sync-requests.php
@@ -1,5 +1,5 @@
 <?php
 /* Add a second delay between bulk-sync requests where the 1 in wp_1_fusewp_bg_process_seconds_between_batches is the blog ID which changes in multisite. */
-add_filter('wp_1_fusewp_bg_process_seconds_between_batches', function () {
+add_filter('wp_1_fusewp_bg_process_seconds_between_batches', function ($seconds) {
   return 1;
 });
